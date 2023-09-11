39f02cb7a425eae2783ee54620c07d082b0bc635
yaolx <64489456@qq.com>
2023-09-11 09:44:59 +0800

    feat：组件库publish验证

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import libCss from 'vite-plugin-libcss'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [libCss(), react()],
   resolve: {
     alias: {
       '@/': path.resolve(__dirname, './src')
